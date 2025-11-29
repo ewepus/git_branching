47de63542e00cf0c75970c6c742c501069d377fe
ewepus <billshifr51@gmail.com>
2025-11-29 21:31:06 +0300

    added method doNothing() to Main.java

--- Main.java
+++ Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
     }
+
+    public static void doNothing() {
+        return;
+    }
 }
