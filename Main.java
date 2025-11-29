75f03c074612cee3dfaf0378891c1cc525759a10
ewepus <billshifr51@gmail.com>
2025-11-29 22:04:03 +0300

    updated counters in Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
-    private static int counter = 0;
-    private static int globalCounter = 0;
+    private static int counter = 1;
+    private static int globalCounter = 1;
 
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
