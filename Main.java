34080a0d558bdc0d1e51f41f02e7bc5fd0a9da14
ewepus <billshifr51@gmail.com>
2025-11-29 21:33:30 +0300

    added counter fields to Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
 public class Main {
+    private static int counter = 0;
+    private static int globalCounter = 0;
+
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
     }
